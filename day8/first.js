068b7c833781e2eaee4947d36be04ea04eac262d
Ajit-Baniya <baniyaajit1@gmail.com>
2023-11-03 06:41:53 +0545

    js

---
+++ day8/first.js
@@ -0,0 +1,14 @@
+console.log("HELLO BAPS") 
+let x=2
+{
+    console.log(x);
+}
+var number = ["1","2","3"];
+console.log(number);
+
+number.push("2")
+console.log(number)
+
+number.pop();
+number.pop();
+console.log(number)
